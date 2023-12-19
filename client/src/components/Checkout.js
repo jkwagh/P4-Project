3bdc89967e741ba2689ce1de041084ab7db93101
william <willpatterson5802@gmail.com>
2023-12-19 13:18:12 -0500

    Checkout

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+
+
+
+
+
+const Checkout = () => {
+
+
+    return (
+        <div>
+            <NavBar />
+            
+
+          </div>
+
+)
+    
+
+            }
+
+
+export default Checkout;
