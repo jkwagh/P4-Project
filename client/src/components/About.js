321305438d65cd000ff88d00570753cc4e4ce4ed
Johey Hargrett <joheyhargrett@gmail.com>
2023-12-20 18:37:49 -0500

    Co-authored-by: WillP1421 <WillP1421@users.noreply.github.com>

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () =>{
+    return (
+        <div>
+            <h1>This is About Page</h1>
+            <p>lorem ipsum 100</p>
+        </div>
+    );
+}
+export default About
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,53 @@
-import {NavLink} from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 import  './NavBar.css';
 import Login from './Login';
 import Signup from './Signup';
 
 
-const NavBar = () => {
+const NavBar = (props) => {
+    const { loggedIn, email } = props;
+  const navigate = useNavigate();
+
+  const onLoginClick = () => {
+    // Use the navigate function to navigate to the Login page
+    navigate("/login");
+  };
+
+  const onSignupClick = () => {
+    // Use the navigate function to navigate to the Login page
+    navigate("/Signup");
+  };
+
 
     return (
-        <nav> 
+        <div>
+          
+             <nav className="nav"> 
              <NavLink to="/">Home</NavLink>
              <NavLink to="/Order">Order</NavLink>
              <NavLink to="/checkout">Checkout</NavLink>
-             {/* <NavLink to="/Login" className={Login}>Login</NavLink>
-             <NavLink to="/Signup" className={Signup}>Signup</NavLink> */}
+             {/* <NavLink to="/Login" className= "Login">Login</NavLink>
+             <NavLink to="/Signup" className="Signup">Signup</NavLink>  */}
+             <NavLink to="/About">About</NavLink>
+             
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onLoginClick}
+          value={loggedIn ? "Log out" : "Log in"}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+        <input
+          className={"inputButton-signup"}
+          type="button"
+          onClick={onSignupClick}
+          value="Signup"
+        />
+        
+             
         </nav>
+        </div>
+       
     );
 }
 
